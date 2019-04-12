5496622f99c1ab0b8bde02c68e59bac646e4d6f8
Listen.Li <listioning@163.com>
2019-04-12 15:40:04 +0800

    upload

--- classes/iptables.h
+++ classes/iptables.h
@@ -7,13 +7,9 @@ static bool can_access(char *szIp){
 	#ifndef WIN32
 	/* PROTECT IP  check ip for access */
 	/*
-	 *	61.130.182.122
-	 *	120.199.11.146
 	 *  127.0.0.1
 	 */
-	if(stricmp(szIp,"61.130.182.122")
-		&&stricmp(szIp,"120.199.11.146")
-		&&!strstr(szIp,"192.168.18.")
+	if(!strstr(szIp,"192.168.18.")
 		&&stricmp(szIp,"127.0.0.1")){
 		return false;
 	}
